4850771d21390e0ed5333fee92d4eb1b8811eb1e
Hassel2 <hasselprassel@mail.ru>
2023-11-19 00:47:45 +0300

    feat(treesitter.lua): delete treesitter auto disable

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,16 +1,10 @@
 local configs = require('nvim-treesitter.configs')
 configs.setup {
-  -- ensure_installed = 'all',
-  -- auto_install = false, -- Do not automatically install missing parsers when entering buffer
+  auto_install = true,
+  sync_install = true,
   highlight = {
     enable = true,
-    disable = function(_, buf)
-      local max_filesize = 100 * 1024 -- 100 KiB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
+    additional_vim_regex_highlighting = false,
   },
   textobjects = {
     select = {
