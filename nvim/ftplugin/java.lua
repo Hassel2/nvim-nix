e85836adc138a430634d5fca842df977d4c9ba61
Hassel2 <hasselprassel@mail.ru>
2023-11-26 13:17:49 +0300

    feat: add initial java lsp configuration

---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,15 @@
+local jdtls_cmd = 'jdt-language-server'
+
+-- Check if jdt-language-server is available
+if vim.fn.executable(jdtls_cmd) ~= 1 then
+  return
+end
+
+vim.lsp.start {
+  name = 'jdtls',
+  cmd = { jdtls_cmd, "-data", "./.jdtls" },
+  root_dir = function() 
+    return vim.fs.dirname(vim.fs.find({ "gradlew" }, { upward = true })[1]);
+  end,
+  capabilities = require('user.lsp').make_client_capabilities(),
+}
