cf12416e1cd68ca55132401f248b7755db822bcb
Hassel2 <hasselprassel@mail.ru>
2023-11-27 00:53:06 +0300

    fix(ftplugin/java.lua): delete extra semicolon

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -9,7 +9,7 @@ vim.lsp.start {
   name = 'jdtls',
   cmd = { jdtls_cmd, "-data", "./.jdtls" },
   root_dir = function()
-    return vim.fs.dirname(vim.fs.find({ "gradlew" }, { upward = true })[1]);
+    return vim.fs.dirname(vim.fs.find({ "gradlew" }, { upward = true })[1])
   end,
   autostart = true,
   capabilities = require('user.lsp').make_client_capabilities(),
