985b6ab860473b1c4a42cdbd6d29a21dee4b07c1
Hassel2 <hasselprassel@mail.ru>
2023-11-26 13:16:32 +0300

    feat(keymaps.lua): delete some default keymaps and add keymap for oil opening

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -12,59 +12,6 @@ keymap.set('n', ']b', vim.cmd.bnext, { silent = true, desc = 'next buffer' })
 keymap.set('n', '[B', vim.cmd.bfirst, { silent = true, desc = 'first buffer' })
 keymap.set('n', ']B', vim.cmd.blast, { silent = true, desc = 'last buffer' })
 
--- Toggle the quickfix list (only opens if it is populated)
-local function toggle_qf_list()
-  local qf_exists = false
-  for _, win in pairs(fn.getwininfo() or {}) do
-    if win['quickfix'] == 1 then
-      qf_exists = true
-    end
-  end
-  if qf_exists == true then
-    vim.cmd.cclose()
-    return
-  end
-  if not vim.tbl_isempty(vim.fn.getqflist()) then
-    vim.cmd.copen()
-  end
-end
-
-keymap.set('n', '<C-c>', toggle_qf_list, { desc = 'toggle quickfix list' })
-
-local function try_fallback_notify(opts)
-  local success, _ = pcall(opts.try)
-  if success then
-    return
-  end
-  success, _ = pcall(opts.fallback)
-  if success then
-    return
-  end
-  vim.notify(opts.notify, vim.log.levels.INFO)
-end
-
--- Cycle the quickfix and location lists
-local function cleft()
-  try_fallback_notify {
-    try = vim.cmd.cprev,
-    fallback = vim.cmd.clast,
-    notify = 'Quickfix list is empty!',
-  }
-end
-
-local function cright()
-  try_fallback_notify {
-    try = vim.cmd.cnext,
-    fallback = vim.cmd.cfirst,
-    notify = 'Quickfix list is empty!',
-  }
-end
-
-keymap.set('n', '[c', cleft, { silent = true, desc = 'cycle quickfix left' })
-keymap.set('n', ']c', cright, { silent = true, desc = 'cycle quickfix right' })
-keymap.set('n', '[C', vim.cmd.cfirst, { silent = true, desc = 'first quickfix entry' })
-keymap.set('n', ']C', vim.cmd.clast, { silent = true, desc = 'last quickfix entry' })
-
 local function lleft()
   try_fallback_notify {
     try = vim.cmd.lprev,
@@ -123,9 +70,6 @@ keymap.set('c', '%%', function()
   end
 end, { expr = true, desc = "expand to current buffer's directory" })
 
-keymap.set('n', '<space>tn', vim.cmd.tabnew, { desc = 'new tab' })
-keymap.set('n', '<space>tq', vim.cmd.tabclose, { desc = 'close tab' })
-
 local severity = diagnostic.severity
 
 keymap.set('n', '<space>e', function()
@@ -181,15 +125,7 @@ keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'move up half-page and center' })
 keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'move down full-page and center' })
 keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'move up full-page and center' })
 
---- Disabled keymaps [enable at your own risk]
-
--- Automatic management of search highlight
--- XXX: This is not so nice if you use j/k for navigation
--- (you should be using <C-d>/<C-u> and relative line numbers instead ;)
---
--- local auto_hlsearch_namespace = vim.api.nvim_create_namespace('auto_hlsearch')
--- vim.on_key(function(char)
---   if vim.fn.mode() == 'n' then
---     vim.opt.hlsearch = vim.tbl_contains({ '<CR>', 'n', 'N', '*', '#', '?', '/' }, vim.fn.keytrans(char))
---   end
--- end, auto_hlsearch_namespace)
+-- Toggle mini.files
+keymap.set('n', '<C-t>', function() 
+      require('oil').open_float()
+end)
