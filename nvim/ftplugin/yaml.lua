2b31b8acd612a270cc1f4019967aae5acc84b120
Hassel2 <hasselprassel@mail.ru>
2024-01-09 21:33:20 +0300

    feat: disable ansiblels and enable yamlls

--- nvim/ftplugin/yaml.lua
+++ nvim/ftplugin/yaml.lua
@@ -1,27 +1,27 @@
-vim.api.nvim_create_autocmd({ "bufnewfile", "bufread", "bufwinenter" }, {
-    pattern = { "*/*ansible*/*.yml" },
-    callback = function()
-        vim.opt.filetype = "yaml.ansible"
-        vim.lsp.start {
-            name = 'ansiblels',
-            cmd = { 'ansible-language-server', '--stdio'},
-            autostart = true,
-            capabilities = require('user.lsp').make_client_capabilities(),
-        }
-    end,
-})
+-- vim.api.nvim_create_autocmd({ "bufnewfile", "bufread", "bufwinenter" }, {
+--     pattern = { "*/*ansible*/*.yml" },
+--     callback = function()
+--         vim.opt.filetype = "yaml.ansible"
+--         vim.lsp.start {
+--             name = 'ansiblels',
+--             cmd = { 'ansible-language-server', '--stdio'},
+--             autostart = true,
+--             capabilities = require('user.lsp').make_client_capabilities(),
+--         }
+--     end,
+-- })
 
 local yamlls_cmd = 'yaml-language-server'
 
 -- Check if yaml-language-server is available
--- if vim.fn.executable(yamlls_cmd) ~= 1 then
---   return
--- end
--- 
--- vim.lsp.start {
---   name = 'yamlls',
---   cmd = { yamlls_cmd, "--stdio"},
---   root_dir = vim.fn.getcwd(),
---   autostart = true,
---   capabilities = require('user.lsp').make_client_capabilities(),
--- }
+if vim.fn.executable(yamlls_cmd) ~= 1 then
+  return
+end
+
+vim.lsp.start {
+  name = 'yamlls',
+  cmd = { yamlls_cmd, "--stdio"},
+  root_dir = vim.fn.getcwd(),
+  autostart = true,
+  capabilities = require('user.lsp').make_client_capabilities(),
+}
